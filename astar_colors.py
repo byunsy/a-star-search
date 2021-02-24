b0a734aa78c65ad8938c46a1d46fba06521288a7
unknown <bsyiskl@gmail.com>
2021-02-24 21:00:25 +0900

    initial commit

---
+++ astar_colors.py
@@ -0,0 +1,13 @@
+# Declaring colors
+RED     = (255, 0, 0) 
+GREEN   = (0, 255, 0)
+BLUE    = (0, 0, 255)
+
+YELLOW  = (255, 255, 0)
+MAGENTA = (255, 0, 255)
+CYAN    = (0, 255, 255)
+ORANGE  = (255, 165, 0)
+
+GREY    = (128, 128, 128)
+WHITE   = (255, 255, 255)
+BLACK   = (0, 0, 0)
